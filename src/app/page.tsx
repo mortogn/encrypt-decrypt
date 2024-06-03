007b7c54e85a1091f6bb86f1adff0f753dfb3776
Amorto Goon <amorto.goon.121@gmail.com>
2024-06-03 22:54:49 +0600

    fixed home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex items-center flex-col gap-8">
       <h1 className="text-5xl font-semibold">Hello Mr. Na..l?</h1>
-      <p className="text-lg">Let's try encryption and decryption.</p>
+      <p className="text-lg">Let&apos; try encryption and decryption.</p>
 
       <div className="flex items-center gap-2">
         <Link className={buttonVariants({ variant: "default" })} href="/encrypt">
